ad727a4ff24f63704cac637df5af67aa1175dfc8
Jed Menard <jmenard@mymail.mines.edu>
2016-02-24 19:48:15 -0700

    ACTUALLY added todo lists haha.

--- src/BoardCell.java
+++ src/BoardCell.java
@@ -1,4 +1,5 @@
 // Represents one cell in the grid
 public class BoardCell {
 	public int row, column = 0;
+	// TODO: I don't think anything more needs to be done to this for now
 }
--- src/IntBoard.java
+++ src/IntBoard.java
@@ -4,23 +4,23 @@ import java.util.Set;
 
 // Contains the grid and adjacency lists
 public class IntBoard {
-	
+
 	private Set<BoardCell> targets = new HashSet<BoardCell>(); // Set of potential targets to move to
 	private LinkedList<BoardCell> adjList = new LinkedList<BoardCell>();	// List of adjacent cells
-	
-	public LinkedList<BoardCell> getAdjList() {
-		return adjList;
-	}
+
 	private void calcAdjacencies() {
-		
+		// TODO: actually find the list of adjacent cells
 	}	// Calculates the adjacency list for each grid cell, stores in a map
+
 	private void calcTargets(BoardCell startCell, int pathLength) {
-		
-	} // Calculates the targets that are pathLength away from the startCell, stores in local variable
-	
+		// TODO: actually find the target cells
+	} 	// Calculates the targets that are pathLength away from the startCell, stores in local variable
+
 	public Set<BoardCell> getTargets() {
 		return targets;
 	}
-	
-	
+
+	public LinkedList<BoardCell> getAdjList() {
+		return adjList;
+	}
 }
