14378f05a2f02f7eab1ec410522cbcc794605027
Jed Menard <jmenard@mymail.mines.edu>
2016-02-24 19:57:41 -0700

    Updated TODO comments to clarify which should be done tomorrow rather than today.

--- src/IntBoard.java
+++ src/IntBoard.java
@@ -8,19 +8,28 @@ public class IntBoard {
 	private Set<BoardCell> targets = new HashSet<BoardCell>(); // Set of potential targets to move to
 	private LinkedList<BoardCell> adjList = new LinkedList<BoardCell>();	// List of adjacent cells
 
+	// Calculates the adjacency list for each grid cell, stores in a map
 	private void calcAdjacencies() {
-		// TODO: actually find the list of adjacent cells
-	}	// Calculates the adjacency list for each grid cell, stores in a map
+		// TODO: actually find the list of adjacent cells IN PART II
+	}	
 
+	// Calculates the targets that are pathLength away from the startCell, stores in local variable
 	private void calcTargets(BoardCell startCell, int pathLength) {
-		// TODO: actually find the target cells
-	} 	// Calculates the targets that are pathLength away from the startCell, stores in local variable
+		// TODO: actually find the target cells IN PART II
+	}	
+
+	public IntBoard(int rows, int cols) {
+		super();
+		// TODO: make this construct the board IN PART II
+	}
 
 	public Set<BoardCell> getTargets() {
-		return targets;
+		return null;
+		// TODO: make this work IN PART II
 	}
 
 	public LinkedList<BoardCell> getAdjList() {
-		return adjList;
+		return null;
+		// TODO: make this work IN PART II
 	}
 }
