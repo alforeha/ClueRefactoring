745fae5a686c34a6ad17204552aeb94fbba8bd5e
Clayton Howeth <choweth@mymail.mines.edu>
2016-03-24 17:30:29 -0600

    testDisproveSuggtion() passes!

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -2,7 +2,9 @@ package clueGame;
 
 import java.awt.Color;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 public class Player {
@@ -23,7 +25,25 @@ public class Player {
 	}
 
 	public Card disproveSuggestion(Solution suggestion){
-		return new Card(CardType.ROOM, "Batroom");
+		
+		ArrayList<Card> possibilities = new ArrayList<Card>();
+		
+		for(Card c : myCards){
+			if(suggestion.person.equals(c.getName()))
+				possibilities.add(c);
+			if(suggestion.weapon.equals(c.getName()))
+				possibilities.add(c);
+			if(suggestion.room.equals(c.getName()))
+				possibilities.add(c);
+		}
+		
+		if(possibilities.size()>0){
+			Random rand = new Random();
+			int randCard = rand.nextInt(possibilities.size());
+			return possibilities.get(randCard);
+		}
+		
+		return null;
 	}
 
 	public Color getColor() {
