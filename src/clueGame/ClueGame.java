f23178bbd0fd0d5f2205417c46e9ab28fe32f5be
Clayton Howeth <choweth@mymail.mines.edu>
2016-04-04 12:58:58 -0600

    Added Dialog box for Detective notes. Only one cell in notes

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -12,8 +12,11 @@ public class ClueGame extends JFrame{
 	public ClueGame(){
 		board = new Board();
 		board.initialize();
-		this.setSize(810, 840);
+		this.setSize(1060, 840);
 		this.add(board);
+		DetectiveNotes d = new DetectiveNotes();
+		d.setSize(500,500);
+		d.setVisible(true);
 	}
 	
 	public static void main(String [] args){
